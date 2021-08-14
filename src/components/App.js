31839a2a50d05244143299c3bb2a393022a05be0
simonojok19 <simonojok19@gmail.com>
2021-08-14 19:19:48 +0300

    fixed the bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "./about/HomePage";
-import AboutPage from "./home/AboutPage";
-import Header from "./common/Header";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <div className="container-fluid">
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-          </Switch>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+  return <h1>Hello</h1>;
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import { render } from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("app"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
