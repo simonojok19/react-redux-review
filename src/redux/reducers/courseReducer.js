2ca17904e653b6163bf020947685413b556d64ad
simonojok19 <simonojok19@gmail.com>
2021-08-14 22:47:32 +0300

    creating the reduces

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,7 +3,10 @@ import { COURSE_ACTION } from "../actions/courseActions";
 export default function courseReducer(state = [], action) {
   switch (action.type) {
     case COURSE_ACTION.CREATE_COURSE: {
-      return [...state, action.course];
+      return [...state, { ...action.course }];
+    }
+    default: {
+      return state;
     }
   }
 }
