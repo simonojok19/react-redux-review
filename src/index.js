50f44977d286b326b532c2c30b3e167a1497c4da
simonojok19 <simonojok19@gmail.com>
2021-08-14 22:57:51 +0300

    connected to the store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ReduxProvider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReduxProvider>,
   document.getElementById("app")
 );
