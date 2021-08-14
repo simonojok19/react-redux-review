675f94c1c0d6fc3101f7fdcb52e15a53e24a92ad
simonojok19 <simonojok19@gmail.com>
2021-08-14 22:33:22 +0300

    created the action

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -12,9 +12,14 @@ export default class CoursesPage extends React.Component {
     this.setState({ course: course });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         <input
---
+++ src/redux/actions/courseActions.js
@@ -0,0 +1,7 @@
+export const COURSE_ACTION = {
+  CREATE_COURSE: "CREATE_COURSE",
+};
+
+export function createCourse(course) {
+  return { type: COURSE_ACTION.CREATE_COURSE, course: course };
+}
