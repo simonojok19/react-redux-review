84620814eae8de95cb67bbe353433ea84c1ee2ff
simonojok19 <simonojok19@gmail.com>
2021-08-14 18:53:47 +0300

    finished the about page

---
+++ src/components/home/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This app uses React, Redux, React Router, and many other helpful</p>
+    </div>
+  );
+}
