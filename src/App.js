69f6e8f2055e09b2d126262cea9a188e7348bb52
simonojok19 <simonojok19@gmail.com>
2021-08-14 19:02:15 +0300

    added index css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <h1>Hello World</h1>
+    </BrowserRouter>
+  );
 }
