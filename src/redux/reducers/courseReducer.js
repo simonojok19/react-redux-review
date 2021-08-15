1eadc893cd19e63b14de932c9658b054acd3e33f
simonojok19 <simonojok19@gmail.com>
2021-08-15 12:07:47 +0300

    cleaned the initail state errors and app running cool

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,7 @@
 import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "../actions/actionTypes";
 import initialState from "./initialState";
 
-export default function courseReducer(state = initialState, action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case CREATE_COURSE: {
       return [...state, { ...action.course }];
