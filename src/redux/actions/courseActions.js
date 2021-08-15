eb2eae114f62ab0006854e3fea14aceb93b6cfec
simonojok19 <simonojok19@gmail.com>
2021-08-15 10:56:47 +0300

    added the thunk for loading courses

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,7 +1,25 @@
+import * as courseApi from "../../api/courseApi";
+
 export const COURSE_ACTION = {
   CREATE_COURSE: "CREATE_COURSE",
+  LOAD_COURSES_SUCCESS: "LOAD_COURSES_SUCCESS",
 };
 
-export function createCourse(course) {
-  return { type: COURSE_ACTION.CREATE_COURSE, course };
+export function createCourse(courses) {
+  return { type: COURSE_ACTION.CREATE_COURSE, courses };
+}
+
+export function loadCoursesSuccess(courses) {
+  return { type: COURSE_ACTION.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return (dispatch) => {
+    return courseApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => throw error);
+  };
 }
